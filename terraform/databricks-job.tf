58ee3be3719d256c04eb37bda3d62aec1fe59b33
king-ak <akdavey@email.wm.edu>
2024-06-11 10:49:18 -0400

    Update TF

--- terraform/databricks-job.tf
+++ terraform/databricks-job.tf
@@ -2,26 +2,32 @@ data "databricks_node_type" "smallest" {
   local_disk = true
 }
 
-data "databricks_spark_version" "latest_lts" {
+data "databricks_spark_version" "latest_ml_lts" {
   long_term_support = true
+  ml                = true
+}
+
+resource "databricks_cluster" "single_node_ml_training" {
+  cluster_name            = "single-node-cluster"
+  spark_version           = data.databricks_spark_version.latest_ml_lts.id
+  node_type_id            = data.databricks_node_type.smallest.id
+  autotermination_minutes = 10
+  num_workers             = 0
+
+  spark_conf = {
+    "spark.databricks.cluster.profile"                                              = "singleNode"
+    "spark.master"                                                                  = "local[*]"
+    format("fs.azure.account.key.%s.dfs.core.windows.net", var.poc_storage_account) = format("{{secrets/%s/poc-storage-account-key}}", databricks_secret_scope.nfl_secret_scope.name)
+  }
+
+  custom_tags = {
+    "project"       = "nfl-play-by-play"
+    "ResourceClass" = "SingleNode"
+  }
 }
 
 resource "databricks_job" "nfl_pbp_pipeline" {
   name = "NFL Play by Play ETL Job"
-
-  job_cluster {
-    job_cluster_key = "nfl_pbp_pipeline_cluster"
-    new_cluster {
-      num_workers   = 1
-      spark_version = data.databricks_spark_version.latest_lts.id
-      node_type_id  = data.databricks_node_type.smallest.id
-
-      spark_conf = {
-        format("fs.azure.account.key.%s.dfs.core.windows.net", var.poc_storage_account) = format("{{secrets/%s/poc-storage-account-key}}", databricks_secret_scope.nfl_secret_scope.name)
-      }
-
-    }
-  }
 
   # dynamic "library" {
   #   for_each = ["langchain==0.0.239", "openai==0.27.8", "matplotlib==3.7.2"]
@@ -41,19 +47,18 @@ resource "databricks_job" "nfl_pbp_pipeline" {
   }
 
   task {
-    task_key        = "bronze"
-    job_cluster_key = "nfl_pbp_pipeline_cluster"
+    task_key            = "bronze"
+    existing_cluster_id = databricks_cluster.single_node_ml_training.id
 
     notebook_task {
-      notebook_path = "notebooks/bronze/BronzeIngestion"
-      source        = "Git Provider"
+      notebook_path   = "notebooks/bronze/BronzeIngestion"
+      source          = "Git Provider"
       base_parameters = {
-        storage_account_name = var.poc_storage_account
-        container_name       = var.poc_container_name
-        // TODO: move following to locals
-        catalog_name         = "nfl-play-by-play-project" 
-        bronze_database_name = "bronze"
-        raw_data_relative_path = "landing/nfl_play_by_play"
+        storage_account_name   = var.poc_storage_account
+        container_name         = var.poc_container_name
+        catalog_name           = local.catalog_name
+        bronze_database_name   = local.bronze_database_name
+        raw_data_relative_path = local.scd_landing_path
       }
     }
   }
@@ -67,19 +72,18 @@ resource "databricks_job" "nfl_pbp_pipeline" {
         task_key = "bronze"
       }
 
-      job_cluster_key = "nfl_pbp_pipeline_cluster"
+      existing_cluster_id = databricks_cluster.single_node_ml_training.id
 
       notebook_task {
-        notebook_path = "notebooks/silver/SilverCuration"
-        source        = "Git Provider"
+        notebook_path   = "notebooks/silver/SilverCuration"
+        source          = "Git Provider"
         base_parameters = {
           target_team          = task.value
           storage_account_name = var.poc_storage_account
           container_name       = var.poc_container_name
-          // TODO: move following to locals
-          catalog_name         = "nfl-play-by-play-project"
-          bronze_database_name = "bronze"
-          silver_database_name = "silver"
+          catalog_name         = local.catalog_name
+          bronze_database_name = local.bronze_database_name
+          silver_database_name = local.silver_database_name
         }
       }
     }
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -7,9 +7,16 @@ locals {
   scd_landing_path = format("%s/nfl_play_by_play", local.landing_root)
 
   # Delta DB paths
-  bronze_delta_db_path = format("%s/bronze", local.abfss_root_path)
-  silver_delta_db_path = format("%s/silver", local.abfss_root_path)
-  gold_delta_db_path   = format("%s/gold", local.abfss_root_path)
+  bronze_delta_db_path     = format("%s/bronze", local.abfss_root_path)
+  silver_delta_db_path     = format("%s/silver", local.abfss_root_path)
+  gold_delta_db_path       = format("%s/gold", local.abfss_root_path)
   # Delta Table paths
   bronze_nflpbp_table_path = format("%s/nfl_play_by_play", local.bronze_delta_db_path)
+
+  # Unity Catalog
+  catalog_name         = "nfl-play-by-play-project"
+  bronze_database_name = "bronze"
+  silver_database_name = "silver"
+
+
 }
